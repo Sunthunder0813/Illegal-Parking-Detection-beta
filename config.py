fe31cabf8081158c24339853c9e1b6b96d528820
Joseph Cañete Santander <santanderjoseph13@gmail.com>
2025-12-21 08:58:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ CAM1_URL = "rtsp://192.168.18.2:554/stream"
 CAM2_URL = "rtsp://192.168.18.113:554/stream"
 
 # Path to YOLO model
-MODEL_PATH = "models/yolo11s.pt"
+MODEL_PATH = "models/yolov8s.hef"
 
 # Directory to save violation images
 SAVE_DIR = "static/violations"
