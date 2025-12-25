e2d03725aad54cad8539d9562a0756be15352819
Sunthunder0813 <santanderjoseph13@gmail.com>
2025-12-25 10:13:29 +0800

    debug

--- app.py
+++ app.py
@@ -235,6 +235,8 @@ latest_frames_lock = threading.Lock()
 latest_detection_results = {"Camera_1": None, "Camera_2": None}
 latest_detection_lock = threading.Lock()
 
+DETECTION_INTERVAL = 0.1  # seconds between detections (increase for less CPU, decrease for more FPS)
+
 def detection_worker(cam_name, stream):
     while True:
         frame = stream.get_frame()
